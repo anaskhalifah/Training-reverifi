f93a6bf2732fbfb7138808c7fe7962cbb7dce490
anaskhalifah <anas.khalifah@optimumpartners.co>
2025-08-27 16:09:53 +0300

    feat: add achievements section (4-cards and points and badges UI)

---
+++ src/Achievements/Achievements.jsx
@@ -0,0 +1,65 @@
+import styles from "./Achievements.module.css";
+import { FaInstagram } from "react-icons/fa";
+
+const cards = [
+    {
+        id: 1,
+        image: "src/assets/Achievements/img1.png",
+        title: "Card Title 1",
+        date: "Jan 12, 2025",
+    },
+    {
+        id: 2,
+        image: "src/assets/Achievements/img2.png",
+        title: "Card Title 2",
+        date: "Feb 3, 2025",
+    },
+    {
+        id: 3,
+        image: "src/assets/Achievements/img3.png",
+        title: "Card Title 3",
+        date: "Mar 8, 2025",
+    },
+    {
+        id: 4,
+        image: "src/assets/Achievements/img1.png",
+        title: "Card Title 4",
+        date: "Apr 21, 2025",
+    },
+];
+
+export default function Achievements() {
+    return (
+        <div className={styles.AchievementsSection}>
+            <header><link rel="preconnect" href="https://fonts.googleapis.com" />
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+                <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet" /></header>
+<div className={styles.instagramIcon}><FaInstagram size={40}/></div>
+
+            <div className={styles.headerOfAchievements}>
+                <h1 className={styles.achievementsTitle}>Achievements</h1>
+                <div className={styles.achievementAndBadgeContainer}>
+                    <div className={styles.pointsElement}>
+                        <img src="/src/assets/Achievements/pointsIcon.png" />
+                        <span>3300 Points</span>
+                    </div>
+                    <div className={styles.badgeElement}>
+                        <img src="/src/assets/Achievements/badge.png" />
+                        <span>8 Badges</span>
+                    </div>
+                </div>
+            </div>
+            <section className={styles.cardGrid}>
+                {cards.map((card) => (
+                    <div key={card.id} className={styles.card}>
+                        <img src={card.image} alt={card.title} className={styles.cardImg} />
+                        <div className={styles.cardContent}>
+                            <h4 className={styles.cardTitle}>{card.title}</h4>
+                            <p className={styles.cardDate}>{card.date}</p>
+                        </div>
+                    </div>
+                ))}
+            </section>
+        </div>
+    );
+}
--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Header from './Header/Header'
 import Profile from './Profile/Profile'
+import Achievements from './Achievements/Achievements'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,6 +13,7 @@ function App() {
     <>
     <Header></Header>
     <Profile></Profile>
+    <Achievements></Achievements>
 
     </>
   )
