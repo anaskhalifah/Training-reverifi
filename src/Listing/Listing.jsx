45ce2826a712d0d5f9afcfdf788401b40e96173a
anaskhalifah <anas.khalifah@optimumpartners.co>
2025-08-28 11:01:24 +0300

    feat: complete listings section

--- src/Listing/Listing.jsx
+++ src/Listing/Listing.jsx
@@ -1,74 +1,111 @@
 import { useState } from "react";
 import styles from "./Listing.module.css";
-import { FaBed, FaBath, FaRulerCombined, FaHeart, FaRegCommentDots } from "react-icons/fa";
+import {
+    FaBed,
+    FaBath,
+    FaRulerCombined,
+    FaHeart,
+    FaRegCommentDots,
+} from "react-icons/fa";
+import { TbMessages } from "react-icons/tb";
+import { PiGarageLight } from "react-icons/pi";
+import { CiHeart } from "react-icons/ci";
+import { HiOutlineShare } from "react-icons/hi";
+import { MdOutlineArrowForwardIos, MdOutlineArrowBackIosNew } from "react-icons/md";
 
 export default function Listing() {
-  const [currentIndex, setCurrentIndex] = useState(0);
+    const cards = [
+        { id: 1, img: "/src/assets/Listing/home1.jpg", price: "$450,000", days: 7 },
+        { id: 2, img: "/src/assets/Listing/home2.jpg", price: "$510,000", days: 12 },
+        { id: 3, img: "/src/assets/Listing/home3.jpg", price: "$600,000", days: 2 },
+    ];
 
-  const cards = [
-    { id: 1, img: "/src/assets/listing1.jpg", price: "$450,000", days: 7 },
-    { id: 2, img: "/src/assets/listing2.jpg", price: "$510,000", days: 12 },
-    { id: 3, img: "/src/assets/listing3.jpg", price: "$600,000", days: 2 },
 
-  ];
 
-  const next = () => {
-    if (currentIndex < cards.length - 3) {
-      setCurrentIndex(currentIndex + 1);
-    }
-  };
+    return (
+        <>
+            <header>
+                <link rel="preconnect" href="https://fonts.googleapis.com" />
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+                <link
+                    href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap"
+                    rel="stylesheet"
+                />
+            </header>
 
-  const prev = () => {
-    if (currentIndex > 0) {
-      setCurrentIndex(currentIndex - 1);
-    }
-  };
+            <section className={styles.section}>
+                <h2 className={styles.title}>Listings</h2>
+                <div className={styles.sliderWrapper}>
 
-  return (
-    <section className={styles.section}>
-      <h2 className={styles.title}>Featured Listings</h2>
-      <div className={styles.sliderWrapper}>
-        <button className={styles.arrow} onClick={prev} disabled={currentIndex === 0}>
-          ◀
-        </button>
-        <div className={styles.slider}>
-          <div
-            className={styles.cardsContainer}
-            style={{ transform: `translateX(-${currentIndex * (392 + 20)}px)` }}
-          >
-            {cards.map((card) => (
-              <div key={card.id} className={styles.card}>
-                <img src={card.img} alt="Listing" className={styles.image} />
+                    <div className={styles.slider}>
+                        <div className={styles.cardsContainer} >
+                            <button className={styles.arrow} >
+                                <MdOutlineArrowBackIosNew size={45} color="#b7cf53" />
 
-                <div className={styles.price}>{card.price}</div>
-                <div className={styles.days}>{card.days} days on Reverifi</div>
-                <div className={styles.sale}>Sale</div>
+                            </button>
+                            {cards.map((card) => (
+                                <div key={card.id} className={styles.card}>
+                                    <img src={card.img} alt="Listing" className={styles.image} />
 
-                <div className={styles.stats}>
-                  <span><FaBed /> 3</span>
-                  <span><FaBath /> 2</span>
-                  <span><FaRulerCombined /> 1200 sqft</span>
+                                    <div className={styles.price}>
+                                        <span>{card.price}</span>
+                                        <span>
+                                            <span className={styles.heartIcon}>
+                                                <CiHeart size={24} />{" "}
+                                            </span>
+                                            <span className={styles.shareIcon}>
+                                                <HiOutlineShare size={22} />
+                                            </span>
+                                        </span>
+                                    </div>
+                                    <div className={styles.days}>
+                                        {card.days} days on Reverifi
+                                    </div>
+                                    <div className={styles.sale}>Sale</div>
+
+                                    <div className={styles.stats}>
+                                        <div>
+                                            <span>5 </span>
+                                            <span className={styles.cardIcons}>
+                                                <FaBed size={30} />
+                                            </span>
+                                        </div>
+                                        <div>
+                                            <span>5</span>{" "}
+                                            <span className={styles.cardIcons}>
+                                                <FaBath size={25} />
+                                            </span>
+                                        </div>
+                                        <div>
+                                            <span>3 </span>
+                                            <span className={styles.cardIcons}>
+                                                <PiGarageLight size={30} />
+                                            </span>
+                                        </div>
+                                    </div>
+
+                                    <div className={styles.listFooter}>
+                                        <div className={styles.actions}>
+                                            <span>
+                                                <FaHeart size={25} color="#B7B7BA" /> 4
+                                            </span>
+                                            <span>
+                                                <TbMessages size={25} color="#B7B7BA" /> 2 Views
+                                            </span>
+                                        </div>
+                                        <button className={styles.viewBtn}>View Listing</button>
+                                    </div>
+                                </div>
+                            ))}
+                            <button className={styles.arrow}>
+                                <MdOutlineArrowForwardIos size={45} color="#b7cf53" />
+
+                            </button>
+                        </div>
+                    </div>
+
                 </div>
-
-                <div className={styles.listFooter}>
-                  <div className={styles.actions}>
-                    <span><FaHeart /> 4</span>
-                    <span><FaRegCommentDots /> 2</span>
-                  </div>
-                  <button className={styles.viewBtn}>View Listing</button>
-                </div>
-              </div>
-            ))}
-          </div>
-        </div>
-        <button
-          className={styles.arrow}
-          onClick={next}
-          disabled={currentIndex >= cards.length - 3}
-        >
-          ▶
-        </button>
-      </div>
-    </section>
-  );
+            </section>
+        </>
+    );
 }
